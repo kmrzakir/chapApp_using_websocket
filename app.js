2b644de2c4b0adb47e510638c38a89cc58f30f74
zakir <zakirmustafa444@gmail.com>
2025-01-25 22:28:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("A suer is connected : ");
+
+  // read data send from client
+  socket.on("clent message", (msg) => {
+    console.log("send from client : " + msg);
+    //   send a message to all clients except the send of message
+    socket.broadcast.emit("server message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
